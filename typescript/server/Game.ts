2ee955e4962dc7cdcdf307efca333ab5aabf0b58
Idan Reed <idanclay18554@gmail.com>
2018-10-20 14:38:53 -0500

    worked on attacking

--- typescript/server/Game.ts
+++ typescript/server/Game.ts
@@ -2,11 +2,12 @@ import {PlanetServ} from './PlanetServ';
 import { Link } from '../shared/link';
 import { ResourceType } from '../shared/globals';
 import {Planet} from '../shared/Planet';
+import { Player } from '../shared/player';
 class Game{
     planets : Array<PlanetServ> = new Array<PlanetServ>();
 
     public constructor(playerCount: number, planetCount : number){
-        
+
     }
 
     public update(){
@@ -23,11 +24,31 @@ class Test{
         let testPlanet = PlanetServ.DownCast(testPlanet3);
 
         testPlanet.Receive(10, ResourceType.Labor);
-        testPlanet.outputs.push(new Link(testPlanet, testPlanet2, 1, ResourceType.Labor));
+        testPlanet.outputs.push(new Link(testPlanet, testPlanet2, 1, ResourceType.Labor, 1));
         testPlanet.UpdateOutputs(1);
 
         console.log(testPlanet.buffers.quantities[ResourceType.Labor]);
         console.log(testPlanet2.buffers.quantities[ResourceType.Labor]);
     }
 
+    static testAttack(){
+        let testPlanet = new PlanetServ("TestPlanetName", 1, 1);
+        testPlanet.owner = new Player(1);
+        let testPlanet2 = new PlanetServ("TestPlanetName2", 1, 1);
+        testPlanet2.owner = new Player(2);
+
+        testPlanet.buffers.quantities[ResourceType.Millitary] = 8;
+        testPlanet2.buffers.quantities[ResourceType.Millitary] = 50;
+
+        testPlanet.outputs.push(new Link(testPlanet, testPlanet2, 1, ResourceType.Millitary, 1));
+        testPlanet.UpdateOutputs(1);
+
+        console.log(testPlanet.buffers.quantities[ResourceType.Millitary]);
+        console.log(testPlanet2.buffers.quantities[ResourceType.Millitary]);
+        console.log(testPlanet2.owner.ID);
+        console.log(testPlanet2.partialForceOwner.ID!);
+    }
+
 }
+
+Test.testAttack();
--- typescript/shared/Link.ts
+++ typescript/shared/Link.ts
@@ -2,16 +2,18 @@ import {ResourceType} from "./globals";
 import {Planet} from "./planet";
 
 class Link{
-    public from: Planet;
-    public to: Planet;
-    public rate: number;
-    public type: ResourceType;
+    public readonly from: Planet;
+    public readonly to: Planet;
+    public readonly rate: number;
+    public readonly type: ResourceType;
+    public readonly id: number;
 
-    constructor(fromIn: Planet, toIn: Planet, rateIn: number, typeIn: ResourceType){
+    constructor(fromIn: Planet, toIn: Planet, rateIn: number, typeIn: ResourceType, idIn: number){
         this.from   = fromIn;
         this.to     = toIn;
         this.rate   = rateIn;
         this.type   = typeIn;
+        this.id     = idIn;
     }
 
 }
