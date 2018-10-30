4479ce211b60e4591e2df00fbbc84838e5d76ba3
zachwood0s <zach@hayzak.com>
2018-10-29 23:51:06 -0500

    more test stuff

--- test/HTMLElementBuilder.ts
+++ test/HTMLElementBuilder.ts
@@ -8,13 +8,20 @@ describe("HTMLElementBuilder", () => {
 
     it("Allows for creation of a simple html element", () => {
         let elm = new HTMLElementBuilder("div").Build();
-        expect(elm.tagName).to.equal("div");
+        expect(elm.tagName).to.equal("DIV");
     });
 
     it("Allows for creation of multiple simple html elements", () => {
         let div = new HTMLElementBuilder("div");
         let elm1 = div.Build();
         let elm2 = div.Build();
-        expect(elm1.tagName).to.equal(elm2.tagName).to.equal("div");
+        expect(elm1.tagName).to.equal(elm2.tagName).to.equal("DIV");
+    });
+
+    it("Allows for classes to be added", () => {
+        let elm = new HTMLElementBuilder("div").WithClasses("testClass", "class2").Build();
+        expect(elm.classList.length).to.equal(2);
+        expect(elm.classList).to.contain("testClass");
+        expect(elm.classList).to.contain("class2");
     });
 });
